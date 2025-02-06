871a50b314071293f3858e36f924b0a2de6dd3b7
Kyle Robinson <ky.robinson72@gmail.com>
2025-02-06 14:47:28 -0700

    imported BrowserRouter, NavLink, Route, and Routes. Wrapped all of the HTML in a BrowserRouter and changed Navbar anchors to NavLinks

--- src/app.jsx
+++ src/app.jsx
@@ -2,38 +2,52 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Recipes } from './recipes/recipes';
+import { New } from './new/new';
+import { View } from './view/view';
+
 export default function App() {
   return (
-    <div className='app bg-light text-dark'>
-        <header className="container-fluid">
-            <nav className="navbar fixed-top navbar-custom">
+    <BrowserRouter>
+        <div className='app bg-light text-dark'>
+            <header className="container-fluid">
+                <nav className="navbar fixed-top navbar-custom">
+                    <div className="container-fluid">
+                        <div className="navbar-brand">Let's Eat!</div>
+                        <menu className="navbar-nav">
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="/">
+                                    Home
+                                </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="recipes">
+                                    My Recipes
+                                </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="new">
+                                    Add a Recipe
+                                </NavLink>
+                            </li>
+                        </menu>
+                    </div>
+                </nav>
+            </header>
+
+            <main className="container-fluid bg-white text-center">
+                Placeholder for app components
+            </main>
+
+            <footer className="bg-dark text-white-50">
                 <div className="container-fluid">
-                    <div className="navbar-brand">Let's Eat!</div>
-                    <menu className="navbar-nav">
-                        <li className="nav-item">
-                            <a className="nav-link" href="index.html">Home</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="recipes.html">My Recipes</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="new.html">Add a Recipe</a>
-                        </li>
-                    </menu>
+                    <span className="text-reset">Kyle Robinson</span>
+                    <a className="text-reset" href="https://github.com/robinkyl002/cs260-startup/tree/main">Source</a>
                 </div>
-            </nav>
-        </header>
-
-        <main className="container-fluid bg-white text-center">
-            Placeholder for app components
-        </main>
-
-        <footer className="bg-dark text-white-50">
-            <div className="container-fluid">
-                <span className="text-reset">Kyle Robinson</span>
-                <a className="text-reset" href="https://github.com/robinkyl002/cs260-startup/tree/main">Source</a>
-            </div>
-        </footer>
-    </div>
+            </footer>
+        </div>
+    </BrowserRouter>
   );
 }
