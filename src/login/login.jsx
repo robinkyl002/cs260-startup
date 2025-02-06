a987d65caf21ce48059c4d4e818781b0fc074a92
Kyle Robinson <ky.robinson72@gmail.com>
2025-02-06 15:23:32 -0700

    copied main element code from login.html and put it into the return statement for the Login function

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import React from "react";
 
 export function Login() {
   return (
-    <main className='container-fluid bg-white text-center'>
-      <div>login displayed here</div>
+    <main class="container-fluid bg-white text-center">
+      <h1>Let's get started</h1>
+      <form method="get" action="recipes.html">
+        <div class="input-group mb-3">
+          <span class="input-group-text" id="email">
+            Email:
+          </span>
+          <input type="email" class="form-control" id="email" name="email" />
+        </div>
+        <div class="input-group mb-3">
+          <span class="input-group-text" id="password">
+            Password:
+          </span>
+          <input
+            type="text"
+            class="form-control"
+            aria-label="password"
+            aria-describedby="password"
+          />
+        </div>
+        <button type="submit" class="btn btn-primary">
+          Login
+        </button>
+        <button type="submit" class="btn btn-secondary">
+          Create
+        </button>
+      </form>
     </main>
   );
 }
