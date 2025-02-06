b41505a6f67911e727d89bd45e9c3be4e00cac8c
Kyle Robinson <ky.robinson72@gmail.com>
2025-02-06 15:32:42 -0700

    renamed index.css to login.css and moved it into the login folder, the imported it into the login.jsx file. Also updated all references to class so that they refer to className to avoid issues with JS class object

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,31 +1,38 @@
 import React from "react";
 
+import "./login.css";
+
 export function Login() {
   return (
-    <main class="container-fluid bg-white text-center">
+    <main className="container-fluid bg-white text-center">
       <h1>Let's get started</h1>
       <form method="get" action="recipes.html">
-        <div class="input-group mb-3">
-          <span class="input-group-text" id="email">
+        <div className="input-group mb-3">
+          <span className="input-group-text" id="email">
             Email:
           </span>
-          <input type="email" class="form-control" id="email" name="email" />
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            name="email"
+          />
         </div>
-        <div class="input-group mb-3">
-          <span class="input-group-text" id="password">
+        <div className="input-group mb-3">
+          <span className="input-group-text" id="password">
             Password:
           </span>
           <input
             type="text"
-            class="form-control"
+            className="form-control"
             aria-label="password"
             aria-describedby="password"
           />
         </div>
-        <button type="submit" class="btn btn-primary">
+        <button type="submit" className="btn btn-primary">
           Login
         </button>
-        <button type="submit" class="btn btn-secondary">
+        <button type="submit" className="btn btn-secondary">
           Create
         </button>
       </form>
