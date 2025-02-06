4a42217ab8e2dace9c8202899e8eb14ece2c24e2
Kyle Robinson <ky.robinson72@gmail.com>
2025-02-06 14:32:42 -0700

    changed all class references to className, changed navbar-brand element to a div, and removed content in main element to set up the app to bring in other components

--- src/app.jsx
+++ src/app.jsx
@@ -5,45 +5,33 @@ import './app.css';
 export default function App() {
   return (
     <div className='app bg-light text-dark'>
-        <header class="container-fluid">
-            <nav class="navbar fixed-top navbar-custom">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="#">Let's Eat!</a>
-                    <menu class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link active" href="index.html">Home</a>
+        <header className="container-fluid">
+            <nav className="navbar fixed-top navbar-custom">
+                <div className="container-fluid">
+                    <div className="navbar-brand">Let's Eat!</div>
+                    <menu className="navbar-nav">
+                        <li className="nav-item">
+                            <a className="nav-link" href="index.html">Home</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="recipes.html">My Recipes</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="recipes.html">My Recipes</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="new.html">Add a Recipe</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="new.html">Add a Recipe</a>
                         </li>
                     </menu>
                 </div>
             </nav>
         </header>
 
-        <main class="container-fluid bg-white text-center">
-            <h1>Let's get started</h1>
-            <form method="get" action="recipes.html">
-                <div class="input-group mb-3">
-                    <span class="input-group-text" id="email">   Email:    </span>
-                    <input type="email" class="form-control" id="email" name="email" />
-                </div>
-                <div class="input-group mb-3">
-                    <span class="input-group-text" id="password">Password: </span>
-                    <input type="text" class="form-control" aria-label="password" aria-describedby="password" />
-                </div>
-                <button type="submit" class="btn btn-primary" >Login</button>
-                <button type="submit" class="btn btn-secondary">Create</button>
-            </form>
+        <main className="container-fluid bg-white text-center">
+            Placeholder for app components
         </main>
 
-        <footer class="bg-dark text-white-50">
-            <div class="container-fluid">
-                <span class="text-reset">Kyle Robinson</span>
-                <a class="text-reset" href="https://github.com/robinkyl002/cs260-startup/tree/main">Source</a>
+        <footer className="bg-dark text-white-50">
+            <div className="container-fluid">
+                <span className="text-reset">Kyle Robinson</span>
+                <a className="text-reset" href="https://github.com/robinkyl002/cs260-startup/tree/main">Source</a>
             </div>
         </footer>
     </div>
