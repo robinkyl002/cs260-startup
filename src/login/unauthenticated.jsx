505bdf455660dd43041bc2ba25e96b1a2e4c1ad0
Kyle Robinson <ky.robinson72@gmail.com>
2025-02-13 12:08:19 -0700

    Removed placeholders in email and password fields and changed their labels to match current look of login.jsx

--- src/login/unauthenticated.jsx
+++ src/login/unauthenticated.jsx
@@ -22,22 +22,20 @@ export function Unauthenticated(props) {
     <>
       <div>
         <div className="input-group mb-3">
-          <span className="input-group-text">@</span>
+          <span className="input-group-text">Email</span>
           <input
             className="form-control"
             type="text"
             value={userName}
             onChange={(e) => setUserName(e.target.value)}
-            placeholder="your@email.com"
           />
         </div>
         <div className="input-group mb-3">
-          <span className="input-group-text">🔒</span>
+          <span className="input-group-text">Password</span>
           <input
             className="form-control"
             type="password"
             onChange={(e) => setPassword(e.target.value)}
-            placeholder="password"
           />
         </div>
         <Button
