c4eb28030ebc0e5ce2403b4442347c6fbe63ac3a
Kyle Robinson <ky.robinson72@gmail.com>
2025-02-13 12:27:02 -0700

    Updated login.jsx to display the Unauthenticated view

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 
+import { Unauthenticated } from "./unauthenticated";
+import { AuthState } from "./authState";
+
 import "./login.css";
 
-export function Login() {
+export function Login(userName, authState, onAuthChange) {
   return (
     <main className="container-fluid bg-white text-center">
-      <h1>Let's get started</h1>
-      <form method="get" action="recipes.html">
+      {authState !== AuthState.Unknown && <h1>Let's get started</h1>}
+      {authState === AuthState.Unauthenticated && (
+        <Unauthenticated
+          userName={userName}
+          onLogout={() => onAuthChange(userName, AuthState.Unauthenticated)}
+        />
+      )}
+      {/* <form method="get" action="recipes.html">
         <div className="input-group mb-3">
           <span className="input-group-text" id="email">
             Email:
@@ -37,7 +46,32 @@ export function Login() {
             Create
           </button>
         </span>
-      </form>
+      </form> */}
     </main>
   );
 }
+
+// import React from 'react';
+
+// import { Authenticated } from './authenticated';
+
+// export function Login({ userName, authState, onAuthChange }) {
+//   return (
+//     <main className='container-fluid bg-secondary text-center'>
+//       <div>
+//         {authState !== AuthState.Unknown && <h1>Welcome to Simon</h1>}
+//         {authState === AuthState.Authenticated && (
+//           <Authenticated userName={userName} onLogout={() => onAuthChange(userName, AuthState.Unauthenticated)} />
+//         )}
+//         {authState === AuthState.Unauthenticated && (
+//           <Unauthenticated
+//             userName={userName}
+//             onLogin={(loginUserName) => {
+//               onAuthChange(loginUserName, AuthState.Authenticated);
+//             }}
+//           />
+//         )}
+//       </div>
+//     </main>
+//   );
+// }
