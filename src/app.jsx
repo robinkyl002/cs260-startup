5f7a196f197c5015d4d522c0b5a85814d86b6f4d
Kyle Robinson <ky.robinson72@gmail.com>
2025-02-13 12:29:25 -0700

    Added useStates for userName and authState

--- src/app.jsx
+++ src/app.jsx
@@ -1,64 +1,81 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './app.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./app.css";
 
-import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
-import { Login } from './login/login';
-import { Recipes } from './recipes/recipes';
-import { New } from './new/new';
-import { View } from './view/view';
+import { BrowserRouter, NavLink, Route, Routes } from "react-router-dom";
+import { Login } from "./login/login";
+import { Recipes } from "./recipes/recipes";
+import { New } from "./new/new";
+import { View } from "./view/view";
 
 export default function App() {
+  const [userName, setUserName] = React.useState(
+    localStorage.getItem("userName") || ""
+  );
+  const currentAuthState = userName
+    ? AuthState.Authenticated
+    : AuthState.Unauthenticated;
+  const [authState, setAuthState] = React.useState(currentAuthState);
+
   return (
     <BrowserRouter>
-        <div className='app bg-light text-dark'>
-            <header className="container-fluid">
-                <nav className="navbar fixed-top navbar-custom">
-                    <div className="container-fluid">
-                        <div className="navbar-brand">Let's Eat!</div>
-                        <menu className="navbar-nav">
-                            <li className="nav-item">
-                                <NavLink className="nav-link" to="/">
-                                    Home
-                                </NavLink>
-                            </li>
-                            <li className="nav-item">
-                                <NavLink className="nav-link" to="recipes">
-                                    My Recipes
-                                </NavLink>
-                            </li>
-                            <li className="nav-item">
-                                <NavLink className="nav-link" to="new">
-                                    Add a Recipe
-                                </NavLink>
-                            </li>
-                        </menu>
-                    </div>
-                </nav>
-            </header>
+      <div className="app bg-light text-dark">
+        <header className="container-fluid">
+          <nav className="navbar fixed-top navbar-custom">
+            <div className="container-fluid">
+              <div className="navbar-brand">Let's Eat!</div>
+              <menu className="navbar-nav">
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/">
+                    Home
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="recipes">
+                    My Recipes
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="new">
+                    Add a Recipe
+                  </NavLink>
+                </li>
+              </menu>
+            </div>
+          </nav>
+        </header>
 
-            {/* <main className="container-fluid bg-white text-center">
+        {/* <main className="container-fluid bg-white text-center">
                 Placeholder for app components
             </main> */}
-            <Routes>
-                <Route path='/' element={ <Login /> } exact />
-                <Route path='/recipes' element={ <Recipes /> } />
-                <Route path='/new' element={ <New /> } />
-                <Route path='/view' element={ <View /> } />
-                <Route path='*' element={ <NotFound /> } />
-            </Routes>
+        <Routes>
+          <Route path="/" element={<Login />} exact />
+          <Route path="/recipes" element={<Recipes />} />
+          <Route path="/new" element={<New />} />
+          <Route path="/view" element={<View />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
 
-            <footer className="bg-dark text-white-50">
-                <div className="container-fluid">
-                    <span className="text-reset">Kyle Robinson</span>
-                    <a className="text-reset" href="https://github.com/robinkyl002/cs260-startup/tree/main">Source</a>
-                </div>
-            </footer>
-        </div>
+        <footer className="bg-dark text-white-50">
+          <div className="container-fluid">
+            <span className="text-reset">Kyle Robinson</span>
+            <a
+              className="text-reset"
+              href="https://github.com/robinkyl002/cs260-startup/tree/main"
+            >
+              Source
+            </a>
+          </div>
+        </footer>
+      </div>
     </BrowserRouter>
   );
 }
 
 function NotFound() {
-    return <main className='container-fluid bg-secondary text-center'>404: Return to sender. Address unknown.</main>;
-  }
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      404: Return to sender. Address unknown.
+    </main>
+  );
+}
