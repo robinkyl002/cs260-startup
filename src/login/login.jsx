05180fe3dbef31af35634cc21c399b31a7955011
Kyle Robinson <ky.robinson72@gmail.com>
2025-02-18 15:33:10 -0700

    Added console.log to check the values being passed into Login and imported Authenticated from the authenticated.jsx file

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,52 +1,35 @@
 import React from "react";
 
+import { Authenticated } from "./authenticated";
 import { Unauthenticated } from "./unauthenticated";
 import { AuthState } from "./authState";
 
 import "./login.css";
 
 export function Login(userName, authState, onAuthChange) {
+  console.log(userName);
+  // console.log(authState);
+  // console.log(onAuthChange);
+
   return (
     <main className="container-fluid bg-white text-center">
-      {authState !== AuthState.Unknown && <h1>Let's get started</h1>}
-      {authState === AuthState.Unauthenticated && (
-        <Unauthenticated
-          userName={userName}
-          onLogout={() => onAuthChange(userName, AuthState.Unauthenticated)}
-        />
-      )}
-      {/* <form method="get" action="recipes.html">
-        <div className="input-group mb-3">
-          <span className="input-group-text" id="email">
-            Email:
-          </span>
-          <input
-            type="email"
-            className="form-control"
-            id="email"
-            name="email"
+      <div>
+        {authState !== AuthState.Unauthenticated && <h1>Let's get started</h1>}
+        {authState === AuthState.Authenticated && (
+          <Authenticated
+            userName={userName}
+            onLogout={(loginUserName) =>
+              onAuthChange(loginUserName, AuthState.Unauthenticated)
+            }
           />
-        </div>
-        <div className="input-group mb-3">
-          <span className="input-group-text" id="password">
-            Password:
-          </span>
-          <input
-            type="text"
-            className="form-control"
-            aria-label="password"
-            aria-describedby="password"
+        )}
+        {authState === AuthState.Unauthenticated && (
+          <Unauthenticated
+            userName={userName}
+            onLogin={() => onAuthChange(userName, AuthState.Unauthenticated)}
           />
-        </div>
-        <span id="login-buttons">
-          <button type="submit" className="btn btn-primary">
-            Login
-          </button>
-          <button type="submit" className="btn btn-secondary">
-            Create
-          </button>
-        </span>
-      </form> */}
+        )}
+      </div>
     </main>
   );
 }
