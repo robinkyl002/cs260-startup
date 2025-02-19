134bbe3b56753296950442ab051c525628a15d40
Kyle Robinson <ky.robinson72@gmail.com>
2025-02-18 21:48:02 -0700

    added braces that were missing for parameters in function Login and removed console.log entries

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -6,15 +6,11 @@ import { AuthState } from "./authState";
 
 import "./login.css";
 
-export function Login(userName, authState, onAuthChange) {
-  console.log(userName);
-  // console.log(authState);
-  // console.log(onAuthChange);
-
+export function Login({ userName, authState, onAuthChange }) {
   return (
     <main className="container-fluid bg-white text-center">
       <div>
-        {authState !== AuthState.Unauthenticated && <h1>Let's get started</h1>}
+        {authState !== AuthState.Unknown && <h1>Let's get started</h1>}
         {authState === AuthState.Authenticated && (
           <Authenticated
             userName={userName}
@@ -33,28 +29,3 @@ export function Login(userName, authState, onAuthChange) {
     </main>
   );
 }
-
-// import React from 'react';
-
-// import { Authenticated } from './authenticated';
-
-// export function Login({ userName, authState, onAuthChange }) {
-//   return (
-//     <main className='container-fluid bg-secondary text-center'>
-//       <div>
-//         {authState !== AuthState.Unknown && <h1>Welcome to Simon</h1>}
-//         {authState === AuthState.Authenticated && (
-//           <Authenticated userName={userName} onLogout={() => onAuthChange(userName, AuthState.Unauthenticated)} />
-//         )}
-//         {authState === AuthState.Unauthenticated && (
-//           <Unauthenticated
-//             userName={userName}
-//             onLogin={(loginUserName) => {
-//               onAuthChange(loginUserName, AuthState.Authenticated);
-//             }}
-//           />
-//         )}
-//       </div>
-//     </main>
-//   );
-// }
