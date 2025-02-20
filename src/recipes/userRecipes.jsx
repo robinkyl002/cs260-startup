a7cd496d527f89816ab2d99487ac494551f7114e
Kyle Robinson <ky.robinson72@gmail.com>
2025-02-20 15:37:43 -0700

    Added import statements and the UserRecipes function to handle the component

---
+++ src/recipes/userRecipes.jsx
@@ -0,0 +1,6 @@
+import React, { use } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./recipes.css";
+import { useNavigate } from "react-router-dom";
+
+export function UserRecipes() {}
