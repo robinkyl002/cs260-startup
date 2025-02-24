aff5641323d93e6a39aa245626998db5654f2463
Kyle Robinson <ky.robinson72@gmail.com>
2025-02-24 10:31:57 -0700

    Moved cards for user's recipes to userRecipes.jsx and implemented UserRecipes function

--- src/recipes/userRecipes.jsx
+++ src/recipes/userRecipes.jsx
@@ -3,4 +3,91 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./recipes.css";
 import { useNavigate } from "react-router-dom";
 
-export function UserRecipes() {}
+export function UserRecipes() {
+  return (
+    <div
+      id="user-recipes"
+      className="row row-cols-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 g-6"
+    >
+      <div className="col">
+        <div className="card">
+          <img
+            src="/french-toast.jpg"
+            className="card-img-top my-recipe-images"
+            alt="french toast"
+          />
+          <div className="card-body">
+            <h4 className="card-title">French Toast</h4>
+            <ViewRecipe />
+          </div>
+        </div>
+      </div>
+      <div className="col">
+        <div className="card">
+          <img
+            src="/apple-fritter.jpg"
+            className="card-img-top my-recipe-images"
+            alt="apple fritter"
+          />
+          <div className="card-body">
+            <h4 className="card-title">Apple Fritter</h4>
+            <button className="btn btn-primary">View Recipe</button>
+          </div>
+        </div>
+      </div>
+      <div className="col">
+        <div className="card">
+          <img
+            src="/chocolate-chip-cookies.jpeg"
+            className="card-img-top my-recipe-images"
+            alt="chocolate chip cookies"
+          />
+          <div className="card-body">
+            <h4 className="card-title">Chocolate Chip Cookies</h4>
+            <button className="btn btn-primary">View Recipe</button>
+          </div>
+        </div>
+      </div>
+      <div className="col">
+        <div className="card">
+          <img
+            src="/shepherds-pie.jpg"
+            className="card-img-top my-recipe-images"
+            alt="Shepherd's Pie"
+          />
+          <div className="card-body">
+            <h4 className="card-title">Shepherd's Pie</h4>
+            <button className="btn btn-primary">View Recipe</button>
+          </div>
+        </div>
+      </div>
+      <div className="col">
+        <div className="card">
+          <img
+            src="/Spaghetti.jpg"
+            className="card-img-top my-recipe-images"
+            alt="spaghetti"
+          />
+          <div className="card-body">
+            <h4 className="card-title">Spaghetti</h4>
+            <button className="btn btn-primary">View Recipe</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function ViewRecipe() {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/view");
+  };
+
+  return (
+    <button class="btn btn-primary" onClick={handleClick}>
+      View Recipe
+    </button>
+  );
+}
