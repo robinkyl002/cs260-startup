98b7845d0e44bd2c93b4e1143341fa1d6d112792
Kyle Robinson <ky.robinson72@gmail.com>
2025-02-24 12:22:47 -0700

    Replaced userName placeholder with userName passed in from Recipes

--- src/recipes/activity.jsx
+++ src/recipes/activity.jsx
@@ -2,6 +2,14 @@ import React, { use } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./recipes.css";
 
-export function Activity() {
-  return <div>Activity Here</div>;
+export function Activity(props) {
+  return (
+    <aside id="users">
+      <span>User: {props.userName}</span>
+      <ul>
+        <li>Susan just added Crepes</li>
+        <li>Jason just added Apple Fritter</li>
+      </ul>
+    </aside>
+  );
 }
