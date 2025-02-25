21378fc7872489c50585859ca32e1733d4fe82b9
Kyle Robinson <ky.robinson72@gmail.com>
2025-02-25 14:27:56 -0700

    updated constructor and event functions, added functions for adding and removing handlers, and added instance to export an ActivityNotifier object

--- src/recipes/activityNotifier.js
+++ src/recipes/activityNotifier.js
@@ -12,14 +12,32 @@ class ActivityNotifier {
  constructor() {
     setInterval(() => {
         
+        const userName = 'Fred';
+        this.broadcastEvent(userName, );
     }, 15000);
  }
  broadcastEvent(from, recipe) {
     const event = new EventMessage(from, recipe);
     
+    this.receiveEvent(event);
  }
  
- receiveEvent() {
+ addHandler(handler) {
+    this.handlers.push(handler);
+  }
+
+  removeHandler(handler) {
+    this.handlers.filter((h) => h !== handler);
+  }
+ 
+ receiveEvent(event) {
+    this.events.push(event);
     
+    this.handlers.forEach((handler) => {
+        handler(event);
+    });
  }
 }
+
+const ActivityNotify = new ActivityNotifier();
+export { ActivityNotify };
