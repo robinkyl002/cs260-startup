55a90d382cab6d2ca84c9a5de01c2126434d6ee7
Kyle Robinson <ky.robinson72@gmail.com>
2025-02-27 19:08:41 -0700

    Added functionality to represent websocket by creating and displaying events

--- src/recipes/activity.jsx
+++ src/recipes/activity.jsx
@@ -1,15 +1,49 @@
 import React, { use } from "react";
+import { ActivityNotify } from "./activityNotifier";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./recipes.css";
 
 export function Activity(props) {
+  const [events, setEvent] = React.useState([]);
+
+  React.useEffect(() => {
+    ActivityNotify.addHandler(handleRecipeEvent);
+
+    return () => {
+      ActivityNotify.removeHandler(handleRecipeEvent);
+    };
+  }, []);
+
+  function handleRecipeEvent(event) {
+    setEvent((prevEvents) => {
+      let newEvents = [event, ...prevEvents];
+      if (newEvents.length > 2) {
+        newEvents = newEvents.slice(1, 2);
+      }
+      return newEvents;
+    });
+  }
+
+  function createMessageArray() {
+    const messageArray = [];
+    for (const [i, event] of events.entries()) {
+      console.log(event.recipe);
+      let message = ` added ${event.recipe}`;
+
+      messageArray.push(
+        <li key={i} className="recipe-event">
+          <span className={"recipe-event"}>{event.from.split("@")[0]}</span>
+          {message}
+        </li>
+      );
+    }
+    return messageArray;
+  }
+
   return (
     <aside id="users">
       <span>User: {props.userName.split("@")[0]}</span>
-      <ul>
-        <li>Susan just added Crepes</li>
-        <li>Jason just added Apple Fritter</li>
-      </ul>
+      <ul id="recipe-messages">{createMessageArray()}</ul>
     </aside>
   );
 }
