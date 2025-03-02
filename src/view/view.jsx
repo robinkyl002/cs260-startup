85ee470856f394f9eaa5b7beff0abe446845f2d1
Kyle Robinson <ky.robinson72@gmail.com>
2025-03-01 17:48:26 -0700

    updated addNewLines function to create list items

--- src/view/view.jsx
+++ src/view/view.jsx
@@ -3,38 +3,86 @@ import { useLocation } from "react-router-dom";
 import "./view.css";
 
 export function View(props) {
-  const [userRecipes, setUserRecipes] = React.useState([]);
+  let userRecipes = [];
+  const [imgURL, setImgURL] = React.useState(
+    "data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs="
+  );
+  const [ingredients, setIngredients] = React.useState([]);
+  const [instructions, setInstructions] = React.useState([]);
   const location = useLocation();
 
   const recipeName = location.state.recipeId;
 
-  function getRecipeDetailsLocal(recipe) {
+  function getRecipeDetailsLocal(recipeName) {
     const recipesText = localStorage.getItem("userRecipes");
 
     if (recipesText) {
-      setUserRecipes(JSON.parse(recipesText));
+      userRecipes = JSON.parse(recipesText);
+      // console.log(userRecipes);
+    }
+
+    // console.log(userRecipes[0]);
+    let found = false;
+    for (const [i, recipe] of userRecipes.entries()) {
+      if (recipe.recipeName === recipeName) {
+        found = true;
+
+        setImgURL(recipe.imgUrl);
+        setIngredients(addNewLines(recipe.ingredients));
+        setInstructions(addNewLines(recipe.instructions));
+
+        break;
+      }
     }
   }
 
+  React.useEffect(() => {
+    getRecipeDetailsLocal(recipeName);
+  }, []);
+
+  function addNewLines(input) {
+    const output = [];
+    // let count = 0;
+    let line = "";
+
+    for (let i = 0; i < input.length; i++) {
+      if (input.at(i) == "\n") {
+        // output += "<br />";
+        output.push(<li style={{ listStyleType: "none" }}>{line}</li>);
+
+        line = "";
+        // count++;
+      } else {
+        line += input.at(i);
+      }
+    }
+    // console.log(count);
+    return output;
+  }
+
+  let formattedIngredients = addNewLines(ingredients);
+
   return (
     <main id="recipe-view">
-      <div id="recipe_image">
-        <img id="current-recipe" alt={recipeName} src="/french-toast.jpg" />
+      <div id="recipe-image">
+        <img id="current-recipe" alt={recipeName} src={imgURL} />
       </div>
       <div id="recipe-text-elements">
         <h1>{recipeName}</h1>
         <div id="ingredients" className="recipe-details">
           <h3>Ingredients</h3>
-          <ul>
+          <ul>{ingredients}</ul>
+          {/* <ul>
             <li>Bread</li>
             <li>Eggs</li>
             <li>Cinnamon</li>
             <li>Vanilla</li>
-          </ul>
+          </ul> */}
         </div>
         <div id="instructions" className="recipe-details">
           <h3>Instructions</h3>
-          <ol>
+          <p>{instructions}</p>
+          {/* <ol>
             <li>
               Mix together the eggs, cinnamon, and vanilla in a large, shallow
               bowl
@@ -47,7 +95,7 @@ export function View(props) {
               Cook in the pan until the toast is golden brown, and the egg
               mixture is no longer liquid
             </li>
-          </ol>
+          </ol> */}
         </div>
       </div>
     </main>
