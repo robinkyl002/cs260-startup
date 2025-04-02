621ac0f26b25c07519902895d53c3ad0db2528d9
Kyle Robinson <ky.robinson72@gmail.com>
2025-04-02 13:35:09 -0600

    Added variables for the MongoClient, config file, mongodb url, database and collections

---
+++ service/database.js
@@ -0,0 +1,9 @@
+const { MongoClient } = require('mongodb');
+const config = require('./dbConfig.json');
+
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+const client = new MongoClient(url);
+const db = client.db('startup');
+const userCollection = db.collection('user');
+const recipeCollection = db.collection('recipes');
+
