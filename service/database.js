64910775845e89929583284926069555c4b285e5
Kyle Robinson <ky.robinson72@gmail.com>
2025-04-02 13:42:46 -0600

    added functions to get user by token or email

--- service/database.js
+++ service/database.js
@@ -17,3 +17,11 @@ const recipeCollection = db.collection('recipes');
         process.exit(1);
     }
 })()
+
+function getUser(email) {
+    return userCollection.findOne({ email: email });
+}
+
+function getUserByToken(token) {
+    return userCollection.findOne({ token: token });
+}
