c1f520caf854913349d80ddd8a3c192376770c19
Kyle Robinson <ky.robinson72@gmail.com>
2025-04-12 22:44:24 -0600

    updated functions for login/logout functionality to add users or update users

--- service/index.js
+++ service/index.js
@@ -5,6 +5,7 @@ const uuid = require('uuid');
 const multer = require('multer');
 const fs = require('fs');
 const app = express();
+const DB = require('./database.js');
 
 const authCookieName = "token";
 
@@ -46,6 +47,7 @@ apiRouter.post('/auth/login', async (req, res) => {
     if (user) {
         if (await bcrypt.compare(req.body.password, user.password)) {
             user.token = uuid.v4();
+            await DB.updateUser(user);
             setAuthCookie(res, user.token);
             res.send({ email: user.email });
             return;
@@ -59,6 +61,7 @@ apiRouter.delete('/auth/logout', async (req, res) => {
     const user = await findUser('token', req.cookies[authCookieName]);
     if (user) {
         delete user.token;
+        DB.updateUser(user)
     }
     res.clearCookie(authCookieName);
     res.status(204).end();
@@ -162,7 +165,8 @@ async function createUser(email, password) {
         password: passwordHash,
         token: uuid.v4(),
     };
-    users.push(user);
+    // users.push(user);
+    await DB.addUser(user)
 
     return user;
 }
@@ -170,7 +174,11 @@ async function createUser(email, password) {
 async function findUser(field, value) {
     if (!value) return null;
 
-    return users.find((u) => u[field] === value);
+    // return users.find((u) => u[field] === value);
+    if (field === 'token') {
+        return DB.getUserByToken(value);
+    }
+    return DB.getUser(value)
 }
 
 
